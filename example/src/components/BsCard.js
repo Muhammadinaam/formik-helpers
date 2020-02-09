da8ce85b3009de3399fb1f64867cff3b94af2975
minaammunir <minaammunir@gmail.com>
2020-02-09 09:31:02 +0500

    a

--- example/src/components/BsCard.js
+++ example/src/components/BsCard.js
@@ -13,7 +13,7 @@ export default function BsCard(props) {
             <Card className="mt-3 shadow-sm">
                 <CardHeader className="bg-warning">
                     <b>{props.header}</b>
-                    <button className="btn btn-sm btn-info float-right" onClick={() => { setIsOpen(!isOpen) }}>{isOpen ? 'Collapse' : 'Expand'}</button>
+                    <button className="btn btn-sm btn-info float-right" onClick={() => { setIsOpen(!isOpen) }}>{isOpen ? '-' : '+'}</button>
                 </CardHeader>
                 <Collapse isOpen={isOpen}>
                     <CardBody>
